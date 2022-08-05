3bf2507939f7da438cdaf50a984a3f188a6abb7e
sivadotblog <hello@siva.blog>
2022-08-05 13:02:02 -0400

    updated

--- variables.tf
+++ variables.tf
@@ -14,12 +14,3 @@ variable "az-client-secret" {
   type = string
 }
 
-variable "rgname" {
-  type        = string
-  description = "Name of resource group"
-}
-
-variable "location" {
-  type        = string
-  description = "Azure location of resource group"
-}
